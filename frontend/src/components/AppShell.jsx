8ef1fcd9ee4d8ec46072bf560615d8e4fd6e3e8d
Diyorbek-2010 <it.was.a.joke01@gmail.com>
2026-02-12 22:40:51 +0500

    Update AppShell.jsx

--- frontend/src/components/AppShell.jsx
+++ frontend/src/components/AppShell.jsx
@@ -11,7 +11,7 @@ export default function AppShell({ children }) {
 
         {/* Light-mode background (when NOT dark) */}
         <div className="dark:hidden">
-          <div className="absolute inset-0 bg-white" />
+          <div className="absolute inset-0" />
           <div className="absolute -top-48 left-1/2 h-[520px] w-[520px] -translate-x-1/2 rounded-full bg-purple-400/30 blur-3xl" />
           <div className="absolute top-10 -left-40 h-[520px] w-[520px] rounded-full bg-blue-400/25 blur-3xl" />
           <div className="absolute -bottom-40 right-0 h-[520px] w-[520px] rounded-full bg-pink-400/20 blur-3xl" />
