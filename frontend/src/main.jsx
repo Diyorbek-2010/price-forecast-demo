4f595fe8cf5721d5cf1180e95d9547d467a903b8
Diyorbek-2010 <it.was.a.joke01@gmail.com>
2026-02-12 22:30:02 +0500

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,9 @@ import "./index.css";
 import "./i18n";
 import App from "./App.jsx";
 
+document.documentElement.classList.add("dark");
+
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
       <App />
